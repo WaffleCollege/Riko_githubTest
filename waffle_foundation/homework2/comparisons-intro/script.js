0d44f15ac62b7b2ee831e27e1447d1387dd9a725
Riko Kitajima <r1i2k0o1.k.2000@gmail.com>
2022-11-11 01:00:47 +0900

    add homework2

---
+++ waffle_foundation/homework2/comparisons-intro/script.js
@@ -0,0 +1,76 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+// 比較入門
+// 1.
+console.log(2 + 3 * 10 < 50); // => true
+
+
+// 2.
+function isEqual(valueOne, valueTwo) {
+    return valueOne == valueTwo;
+}
+console.log(isEqual(18, "18")); // => true
+/*
+===は値とデータ型両方の一致を評価し、==は値の一致のみを評価する。
+*/
+
+
+// 3.
+// ①
+function isGreaterThan(valueOne, valueTwo) {
+    return valueOne > valueTwo;
+}
+console.log(isGreaterThan(100, 10)); // => true
+
+// ②
+console.log(isGreaterThan(1, 10)); // => false
+
+
+// 4.
+function isOfAge(age) {
+    return age >= 20;
+}
+console.log(isOfAge(20)); // => true
+console.log(isOfAge(21)); // => true
+console.log(isOfAge(19)); // => false
+
+
+// Lab Day おすすめ問題
+// 1.
+function isEven(n) {
+    if (n % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isEven(2)); // => true
+console.log(isEven(1)); // => false
+console.log(isEven(0)); // => true
+
+
+// 2.
+function rightCircularCylinderVolume(radius, height) {
+    return radius ** 2 * height * 3.14;
+}
+console.log(rightCircularCylinderVolume(5, 3)); // => 235.5
+
+function frustumOfRightCircularConeVolume(radius1, radius2, height) {
+    return ((radius1 ** 2 + radius1 * radius2 + radius2 ** 2) * height * 3.14) / 3;
+}
+console.log(frustumOfRightCircularConeVolume(3, 7, 8)); // => 661.4933333333333
+
+
+// 3.
+function validCredentials(username, password) {
+    if (username.length >= 4 && password.length >=8) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(validCredentials("rikok", "wafflecollege")); // => true
+/*
+lengthは、文字列や配列のために用意されているプロパティ。数値には利用できない。
+*/
---
+++ waffle_foundation/homework2/script.js
@@ -0,0 +1,120 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+// 問題
+// 1.
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+console.log(add(4, 3)); // => 7
+
+console.log(add(100, 42)); // => 142
+
+
+// 2.
+console.log(add(100)); // => NaN
+
+console.log(add(1, 4, 5)); // => 5
+
+
+// 3.
+function simpleFunctionA() {
+    return "Hello simple function A";
+}
+function simpleFunctionB() {
+    console.log("Hello simple function B");
+}
+console.log(simpleFunctionA(), "--> From running Simple Function A"); // => Hello simple function A --> From running Simple Function A
+
+console.log(simpleFunctionB(), "--> From running Simple Function B"); // => Hello simple function B
+/*
+'--> From running Simple Function B'はundefinedとなる
+*/
+
+/*
+return => "返す", console => Google Chromeのconsoleの部分に"表示する"の意味
+
+関数『function simpleFunctionA()』の解説
+初めに、関数 simpleFunctionA()の中身でreturnで文字列の返り値"Hello simple function A"を返している。そして、console.logではまず関数 simpleFunctionA()を呼び出し、その後に文字列"--> From running Simple Function A"をconsoleに表示する、としている。
+そのため、関数 simpleFunctionA()では　Hello simple function A --> From running Simple Function A　と表示される。
+
+関数『function simpleFunctionB()』の解説
+関数 simpleFunctionB()の中身ではreturnで何の返り値も返しておらず、ただconsoleで文字列"Hello simple function B"を表示する、とだけしている。
+そのため、関数 simpleFunctionB()では　Hello simple function B　とだけ表示され、文字列"--> From running Simple Function B"はconsoleに表示されずにundefinedとなる。
+*/
+
+
+// 4.
+function average(num1, num2) {
+    return (num1 + num2) / 2;
+}
+console.log(average(4, 6)); // => 5
+
+console.log(average(100, 42)); // => 71
+
+console.log(average(75,89)); // => 82
+
+
+// 1.
+function square(num1) {
+    return num1 * num1;
+}
+console.log(square(5)); // => 25
+/*
+仮引数と関数の中身、returnの後ろの部分に直接数値を入れているのが誤り。
+
+※Identifier (識別子)
+　識別子 (identifier) は 変数、関数、プロパティ などを識別するコード内の文字の並びです。
+　JavaScript においては、識別子は大文字小文字の区別があり、Unicode の文字、 $、 _、 数字 (0-9) を含めることができますが、数字で始めることはできません。
+　識別子は文字列とは異なり、文字列がデータであるのに対して、識別子はコードの一部です。 JavaScript において、識別子を文字列に変換する方法はありませんが、ときに文字列を識別子に解釈することは可能です。
+*/
+
+function square(x) {
+    return x * x;
+}
+console.log(square(13)); // => 169
+/*
+仮引数と関数の中身、returnの後ろの部分に直接文字列を入れているのが誤り。
+*/
+
+function Square(monkey) {
+    return monkey ** 2;
+}
+console.log(Square(5)); // => 25
+
+
+// 1.
+function cube(x) {
+    return x ** 3;
+}
+console.log(cube(5)); // => 125
+
+
+// 2.
+function waffleMaker(topping) {
+    return topping + " " + "Waffle";
+}
+console.log(waffleMaker("Apple")); // => Apple Waffle
+
+console.log(waffleMaker("Honey")); // => Honey Waffle
+
+
+// 3.
+function Circle(radius){
+    return radius ** 2 * 3.14;
+}
+console.log(Circle(5)); // => 78.5
+
+
+// 理解度チェック
+// 1.
+/*
+関数を実行するとき、仮引数より実引数が少ない場合は、NaNと表示される。
+仮引数より実引数が多い場合は、最初の実引数のみが反映され、その結果が返される。
+*/
+
+
+// 2.
+/*
+return キーワードは、関数の中身を返す。関数の外側で使うことはできない。
+*/
